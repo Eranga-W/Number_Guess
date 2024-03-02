4feab05656b0bf5586176f73c4953554b943bc9f
Eranga-W <wadewickramasinghe@gmail.com>
2024-03-02 11:12:05 +0530

    3rd commit

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ console.log(n);
 function refresh(){
     let n = Math.round(Math.random() * 10);
     console.log(n);
+    document.getElementById("txt").value ="";
+    document.getElementById("display").value ="";
+
 }
 
 let count = 0;
@@ -13,14 +16,12 @@ function guess() {
         count++;
         if (document.getElementById("txt").value == n) {
             document.getElementById("display").value="Won";
-            refresh();
         } else if (document.getElementById("txt").value > n) {
             document.getElementById("display").value="Lower... "+(3-count)+" Chances Left...";
         } else {
             document.getElementById("display").value="Higher... "+(3-count)+" Chances Left...";
         }
     }else{
-        console.log("No any Chances Left...");
         refresh();
         document.getElementById("txt").value ="";
         document.getElementById("display").value ="No any Chances Left...";
